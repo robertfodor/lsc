10f6f869edb8133a72d822e85ae3ecbb763a64b8
Róbert Fodor <fodor.robert@gmail.com>
2022-09-17 09:42:55 +0200

    fisher

--- bookdown/build_book.sh
+++ bookdown/build_book.sh
@@ -2,4 +2,8 @@
 
 set -ev
 
+# Remove "docs" folder from parent directory
+rm -rf ../docs
+
+# Run bookdown
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', output_dir='../docs')"
