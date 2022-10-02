859e8bcf6f96cb1636c184d31095793446763804
Róbert Fodor <fodor.robert@gmail.com>
2022-10-03 00:41:32 +0200

    Latex adjustments

--- bookdown/build_book.sh
+++ bookdown/build_book.sh
@@ -3,15 +3,16 @@
 set -ev
 
 # Remove "docs" folder from parent directory
-rm -rf ../docs
+#rm -rf ../docs
 
 # Run bookdown
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', output_dir='../docs')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', output_dir='../docs')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', output_dir='../docs')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', output_dir='~/Desktop')"
 
 # Create CNAME
-echo 'learningstatisticswithcogstat.com' > ../docs/CNAME
+# echo 'learningstatisticswithcogstat.com' > ../docs/CNAME
 
 # Cleanup
 rm -rf _bookdown_files
 rm -rf _book
+rm -rf lsc.log
